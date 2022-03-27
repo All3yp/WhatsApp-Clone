2692a6db8b679b859d142933bf973d3338632cd9
All3yp <noyallepereira@gmail.com>
2022-03-27 16:47:13 -0300

    Create ChatView with cell

--- WhatsApp Clone/Views/Cells/ChatTableViewCell.swift
+++ WhatsApp Clone/Views/Cells/ChatTableViewCell.swift
@@ -9,15 +9,126 @@ import UIKit
 
 class ChatTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+	// MARK: Views
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+	private var photoImageView: UIImageView = {
+		let imageView = UIImageView()
+		imageView.contentMode = .scaleAspectFill // Deletar dps
+		imageView.clipsToBounds = true
+		return imageView
+	}()
 
-        // Configure the view for the selected state
-    }
+	private let personNameLabel: UILabel = {
+		let label = UILabel()
+		label.numberOfLines = 1
+		label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+		label.textColor = .label
+		return label
+	}()
+
+	private let messageLabel: UILabel = {
+		let label = UILabel()
+		label.numberOfLines = 2
+		label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+		label.textColor = .systemGray
+		return label
+	}()
+
+	private lazy var nameAndMsgStackView: UIStackView = {
+	  let stackView = UIStackView(arrangedSubviews: [personNameLabel, messageLabel])
+	  stackView.axis = .vertical
+	  stackView.alignment = .leading
+	  stackView.distribution = .fill
+	  stackView.spacing = 0
+	  return stackView
+	}()
+
+	private let hourLabel: UILabel = {
+		let label = UILabel()
+		label.numberOfLines = 1
+		label.font = UIFont.systemFont(ofSize: 14, weight: .light)
+		label.textColor = .systemGray
+		return label
+	}()
+
+	private let readLabel: PaddingLabel = {
+		let label = PaddingLabel()
+		label.numberOfLines = 1
+		label.font = .systemFont(ofSize: 13)
+		label.textColor = .white
+		label.backgroundColor = .systemBlue
+		label.clipsToBounds = true
+		return label
+	}()
+
+	func setupCellModel(_ model: ChatCellViewModel) {
+		self.photoImageView.image = UIImage(named: model.image)!
+		self.personNameLabel.text = model.name
+		self.messageLabel.text = model.message
+		self.hourLabel.text = model.hour
+		self.readLabel.text = "\(model.unreadMessagesCount)"
+
+		model.unreadMessagesCount > 0 ? highlightUnreadMessages() : hideUnreadMessages()
+	}
+
+	private func highlightUnreadMessages() {
+		readLabel.isHidden = false
+		hourLabel.textColor = .systemBlue
+	}
+
+	private func hideUnreadMessages() {
+		readLabel.isHidden = true
+		hourLabel.textColor = .systemGray
+	}
+
+	override func didMoveToSuperview() {
+		super.didMoveToSuperview()
+		setupViewCode()
+	}
+
+}
+
+// MARK: Setup View Code
+extension ChatTableViewCell: ViewCode {
+	func buildHierarchy() {
+		self.contentView.addSubviews(
+			photoImageView,
+			nameAndMsgStackView,
+			hourLabel,
+			readLabel
+		)
+		self.contentView.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+	}
+
+	func setupConstraints() {
+		NSLayoutConstraint.activate([
+			photoImageView.heightAnchor.constraint(equalToConstant: 60),
+			photoImageView.widthAnchor.constraint(equalTo: photoImageView.heightAnchor),
+			photoImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+			photoImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+
+			nameAndMsgStackView.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: 20),
+			nameAndMsgStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+			nameAndMsgStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+
+			hourLabel.topAnchor.constraint(equalTo: nameAndMsgStackView.topAnchor),
+			hourLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+			hourLabel.leadingAnchor.constraint(equalTo: nameAndMsgStackView.trailingAnchor, constant: 10),
+
+			readLabel.topAnchor.constraint(equalTo: hourLabel.bottomAnchor, constant: 5),
+			readLabel.trailingAnchor.constraint(equalTo: hourLabel.trailingAnchor)
+		])
+
+		let height = nameAndMsgStackView.heightAnchor.constraint(equalToConstant: 65)
+		height.priority = .defaultHigh
+		height.isActive = true
+	}
+
+	func configureViews() {
+		DispatchQueue.main.async { [unowned self] in
+			self.photoImageView.layer.cornerRadius = self.photoImageView.frame.height / 2
+			self.readLabel.layer.cornerRadius = self.readLabel.frame.height / 2
+		}
+	}
 
 }
--- WhatsApp Clone/Views/ChatView.swift
+++ WhatsApp Clone/Views/ChatView.swift
@@ -9,12 +9,39 @@ import UIKit
 
 class ChatView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+	// MARK: - Views
+	lazy var tableView: UITableView = {
+		let tableView = UITableView()
+		tableView.backgroundColor = .clear
+		return tableView
+	}()
+
+	// MARK: - View lifecycle
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupViewCode()
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+}
+
+// MARK: - Setup View Code
+extension ChatView: ViewCode {
+	func buildHierarchy() {
+		self.addSubviews(tableView)
+		self.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+	}
+
+	func setupConstraints() {
+		NSLayoutConstraint.activate([
+			tableView.topAnchor.constraint(equalTo: self.topAnchor),
+			tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+			tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+			tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+		])
+	}
 
 }
