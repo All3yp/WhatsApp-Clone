e484411605efede174218a46b1276348dbf098ea
All3yp <noyallepereira@gmail.com>
2022-03-27 16:50:13 -0300

    Create Chat Controller with loadview and Mock Model

--- WhatsApp Clone/Controllers/ChatViewController.swift
+++ WhatsApp Clone/Controllers/ChatViewController.swift
@@ -9,7 +9,39 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+	// MARK: - Model
+	var chats: [ChatCellViewModel] = [
+		ChatCellViewModel(
+			id: UUID(),
+			image:  "imageTest",
+			name: "Alley Pereira",
+			message: "Estou realizando um teste aqui rapidinho ta beleza?",
+			hour: "23:23", unreadMessagesCount: 0
+		),
+		ChatCellViewModel(
+			id: UUID(),
+			image:  "imageTest",
+			name: "Alley Pereira",
+			message: "Estou realizando um teste aqui rapidinho ta beleza?",
+			hour: "23:23", unreadMessagesCount: 23
+		),
+		ChatCellViewModel(
+			id: UUID(),
+			image:  "imageTest",
+			name: "Alley Pereira",
+			message: "Estou realizando um teste aqui rapidinho ta beleza?",
+			hour: "23:23", unreadMessagesCount: 23
+		)
+	] {
+		didSet {
+			chatView.tableView.reloadData()
+		}
+	}
 
+	// MARK: - Views
+	private let chatView: ChatView = ChatView(frame: UIScreen.main.bounds)
+
+	// MARK: - View lifecycle
 	init(titleNav: String) {
 		super.init(nibName: nil, bundle: nil)
 		title = titleNav
@@ -19,11 +51,35 @@ class ChatViewController: UIViewController {
 		fatalError("init(coder:) has not been implemented")
 	}
 
+	override func loadView() {
+		super.loadView()
+		self.view = chatView
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		view.backgroundColor = .systemBackground
+
+		chatView.tableView.delegate = self
+		chatView.tableView.dataSource = self
 	}
 
+}
+
+// MARK: - Extensions
+extension ChatViewController: UITableViewDelegate {}
+
+extension ChatViewController: UITableViewDataSource {
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return chats.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = ChatTableViewCell()
+		cell.setupCellModel(chats[indexPath.row])
+		return cell
+	}
 
 }
